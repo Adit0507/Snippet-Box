27067ba66ab00eb164542218f032cc0da49416b4
Adit0507 <adi05072002@gmail.com>
2024-07-20 22:44:00 +0530

    centralized error handling

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,46 +3,38 @@ package main
 import (
 	"fmt"
 	"html/template"
-	// "log"
 	"net/http"
 	"strconv"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-
+		app.notFound(w) // Use the notFound() helper
 		return
 	}
-
 	files := []string{
 		"./ui/html/base.tmpl",
+		"./ui/html/partials/nav.tmpl",
 		"./ui/html/pages/home.tmpl",
-		"./ui/html/partials/nav.templ",
 	}
-
+	
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.errorLog.Print(err.Error())
-		http.Error(w, "Internal server error", 500)
-
+		app.serveError(w, err) // Use the serverError() helper.
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		app.errorLog.Print(err.Error())
-
-		http.Error(w, "Internal server error", 500)
+		app.serveError(w, err) 
 	}
-
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 
 		return
 	}
@@ -50,10 +42,10 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with ID %d..", id)
 }
 
-func (app* application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 
 		return
 	}
