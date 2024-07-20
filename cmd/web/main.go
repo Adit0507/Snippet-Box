206accbd946b2b440d4b51003bb155185b2627e5
Adit0507 <adi05072002@gmail.com>
2024-07-20 11:35:08 +0530

    workin with static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	// file server serves files out of the "./ui/static/" directory.
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+
+	// mux.Handle() registers the file server as the file handler for all
+	// URL paths that start with "/static". For matching paths, we strip
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
