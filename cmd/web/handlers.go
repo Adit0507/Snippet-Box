ed91daf516924d4797a4a6645ebc0d7faa0d86ff
Adit0507 <adi05072002@gmail.com>
2024-07-28 12:37:06 +0530

    fixin up some routing

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,19 +3,16 @@ package main
 import (
 	"errors"
 	"fmt"
+
 	// "html/template"
 	"net/http"
 	"strconv"
 
 	"github.com/Adit0507/Snippet-Box/internal/models"
+	"github.com/julienschmidt/httprouter"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w) // Use the notFound() helper
-		return
-	}
-
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serveError(w, err)
@@ -29,7 +26,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 
@@ -53,12 +52,11 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	app.render(w, http.StatusOK, "view.tmpl", data)
 }
 
-func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
+func (app* application) snippetCreate(w http.ResponseWriter, r*http.Request){
+	w.Write([]byte("Display the form for creating a new snippet...."))
+}
+
+func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	// Create some variables holding dummy data. We'll remove these later on
 	// during the build.
 	title := "O snail"
